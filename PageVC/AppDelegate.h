cb9eeb3e060d6c1d7a18fe3e89d03d9d1951108b
Admin <714439768@qq.com>
2015-05-19 17:37:14 +0800

    Initial Commit

---
+++ PageVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageVC
+//
+//  Created by Admin on 19/5/15.
+//  Copyright (c) 2015年 biz.ourmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
